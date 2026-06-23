a5011f923214409ea427988591526d559b349266
harshS02-coder <hk07hrs2004@gmail.com>
2026-06-24 01:06:52 +0530

    CORS error resolved

--- jawabAI-backend/app.py
+++ jawabAI-backend/app.py
@@ -22,6 +22,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         FRONTEND_URL,
+        "https://multipurpose-jawab-ai.vercel.app",
         "https://*.railway.app",
         "https://*.vercel.app"
     ],
